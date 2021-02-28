682fd8ce61526794a89748cacbd83d3e3842eb07
easymagic <nnamware@yahoo.com>
2021-02-28 15:40:29 +0100

    np

--- app/Http/Controllers/ApiCollectionsController.php
+++ app/Http/Controllers/ApiCollectionsController.php
@@ -9,11 +9,109 @@ class ApiCollectionsController extends Controller
 {
     //
 
+    private $token = '12345abcde';
+
+    function getAuthToken(){
+        return [
+            'token'=>$this->token
+        ];
+    }
+
+    private function notAuthorized(){
+      return request('token') !== $this->token;
+    }
+
     function fetchUsers(){
+
+        if ($this->notAuthorized()){
+            return [
+                'message'=>'Not authorized!',
+                'error'=>true
+            ];
+        }
+
        return [
-           'users'=>User::fetchV2()->get()
+           'users'=>User::fetchV2()->get(),
+           'token'=>request('token')
        ];
     }
+
+
+
+    function getUser($email){
+
+        if ($this->notAuthorized()){
+            return [
+                'message'=>'Not authorized!',
+                'error'=>true
+            ];
+        }
+
+
+        if (!User::fetchV2()->where('email',$email)->exists()){
+             return [
+                 'message'=>'User does not exist!',
+                 'error'=>true,
+                 'user'=>null
+             ];
+         }
+
+         $user = User::fetchV2()->where('email',$email)->first();
+
+         return [
+             'user'=>$user,
+             'token'=>request('token')
+         ];
+
+    }
+
+    function convertJsonToB64(){
+
+        if ($this->notAuthorized()){
+            return [
+                'message'=>'Not authorized!',
+                'error'=>true
+            ];
+        }
+
+
+        $json = request('json');
+        $b64 = base64_encode($json);
+        return [
+            'encoded'=>$b64,
+            'token'=>\request('token')
+        ];
+    }
+
+    function convertb64ToCsv($b64){
+
+        if ($this->notAuthorized()){
+            return [
+                'message'=>'Not authorized!',
+                'error'=>true
+            ];
+        }
+
+
+        $json = base64_decode($b64);
+        $json = json_decode($json,true);
+
+//        return $json;
+
+        $r = [];
+        foreach ($json['user'] as $k=>$v){
+            $r[] = $k . ':' . $v;
+        }
+
+        return [
+            'csv'=>implode(',',$r),
+            'token'=>\request('token')
+        ];
+
+
+    }
+
+
 
 
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+Route::get('authorize',[\App\Http\Controllers\ApiCollectionsController::class,'getAuthToken']);
+
 Route::get('users',[\App\Http\Controllers\ApiCollectionsController::class,'fetchUsers']);
+
+Route::get('get-user/{email}',[\App\Http\Controllers\ApiCollectionsController::class,'getUser']);
+//send json as json-string
+Route::post('json-b64',[\App\Http\Controllers\ApiCollectionsController::class,'convertJsonToB64']);
+Route::get('b64-csv/{b64}',[\App\Http\Controllers\ApiCollectionsController::class,'convertb64ToCsv']);
+
